19893787f374aa62d9fff3e2b3ac38d8b168e8ce
Ian Greulich <ian@greulich.me>
2019-04-24 11:26:51 -0400

    move to remote momnet and jquery

--- app/javascripts/a.require.js
+++ app/javascripts/a.require.js
@@ -1,8 +1,14 @@
 require.config({
   paths: {
-    moment: 'lib/moment',
-    jquery: 'lib/jquery',
-  }
+    // moment: 'lib/moment',
+    // jquery: 'lib/jquery',
+    jquery: 'https://code.jquery.com/jquery-3.4.0.slim.min',
+  },
+  packages: [{
+    name: 'moment',
+    location: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/',
+    main: 'moment'
+  }]
 });
 
 requirejs([
